bd3fd94caf39fd422cafd15112fb3cb1f5071c47
therondcharles <therondcharles@gmail.com>
2017-04-23 19:18:53 +0200

    version7
    page espace bien bien avance
    
    !!!!!!! ATTENTION
    Regarde les modifs de la page add request stp car elles sont importantes

--- src/com/SI/connectUser.java
+++ src/com/SI/connectUser.java
@@ -23,55 +23,148 @@ public class connectUser extends HttpServlet {
 
 		System.out.println("Dans GoogleEsieeServlet, champs reçus: " + mail + mdp);
 
-		// if(mdp.equals(confmdp)){
+		
 		DatastoreService dataStore = DatastoreServiceFactory.getDatastoreService();
-		Transaction tx = dataStore.beginTransaction();
-		tx.commit();
+		
 
 		// Queue queue = QueueFactory.getDefaultQueue();
 		// queue.add(TaskOptions.Builder.withUrl("/tacheDeFond").method(Method.POST).param("kind",
 		// "Personne").param("filterProperty", "age").param("filterValue",
 		// "15"));
-
+		
+		
+		//Recuperation compte client
+		
 		Query q = new Query("user");
 		q.addFilter("mail", FilterOperator.EQUAL, mail);
 		q.addFilter("mdp", FilterOperator.EQUAL, mdp);
 		PreparedQuery pq = dataStore.prepare(q);
 		// String tabuser = "<table><thead></thead><tbody>";
-		String tabuser = "/index.html";
-<<<<<<< HEAD
+		String link = "/index.html";
+
 		String nom="";
 		String type="";
+		String id="";
 		int i =0;
 		
 		for(Entity u:pq.asIterable()){
 			i++;
-			nom=u.getProperty("nom") +" " +  u.getProperty("prenom") +" " +  u.getKey();
-			type=""+u.getProperty("type");
+			nom=u.getProperty("nom") +" " +  u.getProperty("prenom") +" " +  u.getKey().getId();
+			type=u.getProperty("type").toString();
+			id= ""+u.getKey().getId();
 			//tabuser+="<tr><td>"+u.getProperty("nom")+"</td><td>"+u.getProperty("prenom") +"</td><td>"+u.getProperty("mail") +"</td></tr>";
-=======
-		String nom = "";
-		String type = "";
-		int i = 0;
 
-		for (Entity u : pq.asIterable()) {
-			i++;
-			nom = u.getProperty("nom") + " " + u.getProperty("prenom") + " " + u.getKey();
-			type = "" + u.getProperty("type");
-			// tabuser+="<tr><td>"+u.getProperty("nom")+"</td><td>"+u.getProperty("prenom")
-			// +"</td><td>"+u.getProperty("mail") +"</td></tr>";
->>>>>>> origin/master
 		}
 		if (i == 1) {
-			tabuser = "/espace.jsp";
+			link = "/espace.jsp";
 			
 		}
+		
+		String b1="";
+		String b2="";
+		String b3="hidden";
+		String text1="";
+		String text2="";
+		String typeu="";
+		if(type.equals("Client")){
+			text1="un projet";
+			req.setAttribute("typep", "projet");
+			
+			b2="hidden";
+		}
+		
+		if(type.equals("Developpeur")){
+			b1="hidden ";
+			text2="Taches";
+			typeu="tache";
+			
+		}
+		if(type.equals("MOA")){
+			req.setAttribute("typep", "tache");
+			text1="une tache";
+			text2=" Projets";
+			b3="";
+			typeu="projet";
+			
+		}
+		
+		
+		
+		//Recuperation projets utilisateur
+		Query q2 = new Query("request");
+		q2.addFilter("iduser", FilterOperator.EQUAL, id);
+		//q.addFilter("idworker", FilterOperator.EQUAL, id);
+		PreparedQuery pqq = dataStore.prepare(q2);
+		
+int j =0,k=0;
+		String proj="<div class='row'>";
+		for(Entity u:pqq.asIterable()){
+			j++;
+			proj+=" <div class='col-sm-6 col-md-4'><div class='thumbnail'>";
+			proj+="<img src='...' alt='...'>";
+			proj+="<div class='caption'>";
+			proj+=" <h3>"+u.getProperty("nom")+"</h3>";
+			proj+=" <p>"+u.getProperty("description")+"</p>";
+			proj+="<p class='text-center'><a href="+ u.getKey().getId()+" class='btn btn-primary' role='button'>Voir</a> <a href="+ u.getKey().getId()+" class='btn btn-default' role='button'>Terminer</a></p>";
+			proj+="</div> </div> </div> ";
+			if(j==3){
+				j=0;
+				k++;
+				
+				if(k>2){
+					proj+="</div><div id='knowmore'class='row knowmore  hidden '>";
+				}else{
+					
+						proj+="</div><div class='row'>";
+					
+				}
+				
+			}
+		}
+		proj+="</div><p class='text-center'><span onclick=\"ecoute()\" class='btn'> en savoir plus</span></p>";
+		proj+="</div>";
+		
+		
+		
+		//recuperation projet disponibles
+		
+		Query q3 = new Query("request");
+		q3.addFilter("etat", FilterOperator.EQUAL, "soumis");
+		q3.addFilter("type", FilterOperator.EQUAL, typeu);
+		PreparedQuery pq3 = dataStore.prepare(q3);
+			
+		
+		String tablep="";
+		
+		for(Entity u:pq3.asIterable()){
+			
+			tablep+="<tr>"
+					+ "<td>"+u.getProperty("nom")+"</td>"
+					+ "<td>"+u.getProperty("description")+"</td>"
+					+ "<td>"+u.getProperty("delai")+"</td>"
+					+ "<td>"+u.getProperty("cout")+"</td>"
+					+ "<td><a href='"+u.getKey().getId()+"' class='btn'>Valider</a></td>"
+					+ "<td><a href='"+u.getKey().getId()+"' class='btn'>Detail</a></td>"
+					+ "</tr>";
+			
+			
+		}
+		
+		
+		
 
 		// tabuser+="</tbody></table>";
-		req.setAttribute("tabuser", tabuser);
 		req.setAttribute("nom", nom);
+		req.setAttribute("proj", proj);
+		req.setAttribute("block1", b1);
+		req.setAttribute("block2", b2);
+		req.setAttribute("block3", b3);
+		req.setAttribute("text1", text1);
+		req.setAttribute("text2", text2);
+		req.setAttribute("iduser", id);
+		req.setAttribute("tabletache", tablep);
 		// req.setAttribute(" ",tabuser );
-		getServletContext().getRequestDispatcher(tabuser).forward(req, resp);
+		getServletContext().getRequestDispatcher(link).forward(req, resp);
 
 	}
 
--- war/espace.jsp
+++ war/espace.jsp
@@ -6,17 +6,97 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <title>Espace</title>
+
+<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.0.min.js"></script>
 </head>
 <body>
 <div class="container">
 <h1 class="text-center">Page Espace</h1>
 <p> Bonjour <%=request.getAttribute("nom")%>: <br/>
 Bienvenu sur votre dashboard vous pourrez ici consulter vos projets les gerer.</p>
+<h2>Mes projets</h2>
+<%=request.getAttribute("proj")%>
 
+<div class="<%=request.getAttribute("block1")%>">
 
-<%=request.getAttribute("type")%>
+<h2>Ajout d' <%=request.getAttribute("text1")%></h2>
+<div class="row"><div class="text-center container">
+	<div class="col-md-6 col-md-offset-3">
+	<form method="post" action="AddRequest">
+   <div class="form-group">
+    <label >Nom</label>
+    <input type="text" class="hidden" name="iduser" value="<%=request.getAttribute("iduser")%>">
+    <input type="text" class="hidden" name="type" value="<%=request.getAttribute("typep")%>">
+    <input type="text" class="hidden" name="etat" value="soumis">
+    <input type="text" class="form-control" name="nom" placeholder="nom">
+    <label >Description</label>
+    <input type="text" class="form-control" name="description" placeholder="description">
+    <label class="<%=request.getAttribute("block3")%>" >Numero de projet</label>
+    <input type="text" class="form-control <%=request.getAttribute("block3")%>" name="history" placeholder="description">
+  </div>
+  
+
+  <div class="form-group">
+    <label >Delai estimé:</label>
+    <select class="form-control" name="delai">
+    		
+			<option label="court">Court</option>
+			<option label="moyen">Moyen</option> 
+			<option label="long">Long</option> 
+		</select>
+  </div>
+  
+  <div class="form-group">
+    <label >Coût</label>
+    <select class="form-control" name="cout">
+    		
+			<option label="court">Faible</option>
+			<option label="moyen">Moyen</option> 
+			<option label="long">Elevée</option> 
+		</select>
+  </div>
+   
+  <button type="submit" class="btn btn-default btn-primary">Ajout</button>
+  
+</form>
+</div></div></div>
+</div>
+<div class="<%=request.getAttribute("block2")%>">
+
+<h2>Liste des <%=request.getAttribute("text2")%> disponibles</h2>
+<table class="table">
+    <thead>
+      <tr>
+        <th>Nom</th>
+        <th>Description</th>
+        <th>Delai</th>
+        <th>Cout</th>
+      </tr>
+    </thead>
+    <tbody>
+<%=request.getAttribute("tabletache")%>
+	</tbody>
+</div>
 
 </div>
+<script>
+		
+		var checkecoute=0;
+		
+function ecoute(){
+			if (checkecoute==0){
+			$('.knowmore').removeClass("hidden ");
+			
+			checkecoute=1;
+			}
+			else{
+			$('.knowmore').addClass("hidden ");
+			
+			checkecoute=0;
+			}
+};
+</script>
 </body>
 </html>
